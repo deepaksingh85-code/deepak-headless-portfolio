032d6c19a60b04c33fe4f915462e729737033b69
deepak kumar <deepakkumar@deepaks-MacBook-Air.local>
2026-06-12 20:48:07 +0530

    Update WordPress API connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,13 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
-import Services from "./components/Services/Services";
-import About from "./components/About/About";
-import Portfolio from "./components/Portfolio/Portfolio";
-import Testimonials from "./components/Testimonials/Testimonials";
-import Contact from "./components/Contact/Contact";
-import Blog from "./components/Blog/Blog";
-import Footer from "./components/Footer/Footer";
-
 import { fetchAPI } from "@/lib/wordpress";
 
 export default async function Home() {
-  const home = await fetchAPI("/pages?slug=home");
-
-  const acf = home[0].acf;
-const timeline = await fetchAPI("/timeline");
-  // Fetch Hero Image
-  const media = await fetchAPI(`/media/${acf.hero_image}`);
-
-  const heroData = {
-    ...acf,
-    hero_image: media.source_url,
-  };
+  const services = await fetchAPI("/service");
 
   return (
-    <>
-      <Header />
-      <Hero data={heroData} />
-      <Services />
-      <About />
-      <Portfolio />
-      <Testimonials data={timeline}  />
-      <Blog />
-      <Contact />
-      <Footer />
-    </>
+    <div style={{ padding: 40 }}>
+      <h1>Connected Successfully 🚀</h1>
+
+      <pre>{JSON.stringify(services, null, 2)}</pre>
+    </div>
   );
 }
-
-
-
-
