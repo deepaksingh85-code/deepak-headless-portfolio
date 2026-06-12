e9d39272d9d5d086ab758f07e7a4c22f613e7c5a
deepak kumar <deepakkumar@deepaks-MacBook-Air.local>
2026-06-12 20:56:53 +0530

    latest changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,40 @@ import Services from "./components/Services/Services";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonials from "./components/Testimonials/Testimonials";
-import Blog from "./components/Blog/Blog";
 import Contact from "./components/Contact/Contact";
+import Blog from "./components/Blog/Blog";
 import Footer from "./components/Footer/Footer";
 
+import { fetchAPI } from "@/lib/wordpress";
+
 export default async function Home() {
+  const home = await fetchAPI("/pages?slug=home");
+
+  const acf = home[0].acf;
+const timeline = await fetchAPI("/timeline");
+  // Fetch Hero Image
+  const media = await fetchAPI(`/media/${acf.hero_image}`);
+
+  const heroData = {
+    ...acf,
+    hero_image: media.source_url,
+  };
+
   return (
     <>
       <Header />
-      <Hero />
+      <Hero data={heroData} />
       <Services />
       <About />
       <Portfolio />
-      <Testimonials />
+      <Testimonials data={timeline}  />
       <Blog />
       <Contact />
       <Footer />
     </>
   );
 }
+
+
+
+
